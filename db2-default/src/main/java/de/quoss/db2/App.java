7e0208389dfa62c0f40759192204647d40cdb5ec
cquoss <clemens@quoss.de>
2024-02-09 09:17:47 +0100

    Share wip 02/09/2024 09.17.

--- db2-default/src/main/java/de/quoss/db2/App.java
+++ db2-default/src/main/java/de/quoss/db2/App.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package de.quoss.db2;
 
-/**
- *
- * @author clemens
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class App {
+
+    private void run() throws Exception {
+        try (final Connection connection = DriverManager.getConnection("jdbc:db2://localhost:35581/db2inst1", "db2inst1", "db2inst1")) {
+            
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new App().run();
+    }
     
 }
